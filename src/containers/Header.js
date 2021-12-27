293ec3e42a36033f86526ca468f2ff639fd404d4
mkandanmk7 <mkandanmk7@gmail.com>
2021-12-27 10:18:46 +0530

    thunk

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,10 +1,19 @@
+import { ShoppingCart } from "@mui/icons-material";
+import { Badge } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="ui fixed menu">
-      <div className="ui container center">
-        <h2>FakeShop</h2>
+      <div className="ui container center ">
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+          <h2>FakeShop</h2>
+        </Link>
+
+        <Badge badgeContent={4} color="primary">
+          <ShoppingCart />
+        </Badge>
       </div>
     </div>
   );
