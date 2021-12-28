77d9f202d372d61930d3d2ef97de3452b556d87a
mkandanmk7 <mkandanmk7@gmail.com>
2021-12-28 11:58:11 +0530

    count Value changed

--- src/Components/UseCounter.js
+++ src/Components/UseCounter.js
@@ -2,17 +2,16 @@ import { useEffect, useState } from "react";
 
 //custom hooks
 export const UseCounter = (limit, interval) => {
-  const [countValue, setCount] = useState(0);
+  const [countValue, setCount] = useState(1);
   const timer = () => setCount(countValue + 1);
 
   useEffect(() => {
-    if (countValue > limit - 2) return;
+    if (countValue >= limit) return;
     console.log("Count value", countValue);
     const Stop = setInterval(timer, interval);
-    console.log(Stop);
 
     return () => {
-      // console.log("unmount count value:", countValue);
+      console.log("unmount count value:", countValue);
       // console.log("unmounted");
 
       clearInterval(Stop);
